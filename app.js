774d1829a808ee5794974ce25df62acd31b88c7d
BrayanStevenRianoFuentesADSO <brayanstevenriano@gmail.com>
2024-07-18 21:36:41 -0500

    agregamos validacion de campos huesped

---
+++ app.js
@@ -0,0 +1,44 @@
+let dom=document
+
+let id=dom.querySelector("#id")
+let nombre =dom.querySelector("#nombre")
+let telefono=dom.querySelector("#telefono")
+
+id.addEventListener('input', function(event){
+    let regexp=/^[0-9]+$/
+
+    if(regexp.test(id.value)){
+        console.log("se ha ingresado un numero")
+    }
+
+    else{
+        id.value=id.value.replace(/[^0-9]$/, '');
+        console.log("no se ha ingresado un numero")
+    }
+})
+
+telefono.addEventListener('input', function(event){
+    let regexp=/^[0-9]+$/
+
+    if(regexp.test(telefono.value)){
+        console.log("se ha ingresado un numero")
+    }
+
+    else{
+        telefono.value=telefono.value.replace(/[^0-9]$/, '');
+        console.log("no se ha ingresado un numero")
+    }
+})
+
+nombre.addEventListener('input', function(event){
+    let regexp=/^[a-zA-Z]+$/
+
+    if(regexp.test(nombre.value)){
+        console.log("se ha ingresado un numero")
+    }
+
+    else{
+        nombre.value=nombre.value.replace(/[^a-zA-Z]$/, '');
+        console.log("no se ha ingresado un numero")
+    }
+})
